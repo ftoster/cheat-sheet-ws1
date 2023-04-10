16e0d6750e7284b42a93b6ef52417cc9d794ed69
toster28 <130085270+toster28@users.noreply.github.com>
2023-04-10 21:44:56 +0300

    Update save.sh

--- save.sh
+++ save.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-find . -type d -maxdepth 1 -mindepth 1| xargs -i tar -cvzf {}.tar.gz {}
+
+# Задаем имя пользователя и адрес удаленного сервера
+remote_user="remote_user"
+remote_host="remote_host"
+
+# Создаем папку для архивов
+mkdir -p archive
+
+# Архивируем все папки с лог-файлами
+for dir in */ ; do
+  if [ -d "$dir" ]; then
+    tar -zcvf "archive/${dir%/}.tar.gz" "$dir"
+  fi
+done
+
+# Копируем папку с архивами на удаленный сервер
+rsync -avz --delete archive/ "${remote_user}@${remote_host}:home/"
+
+# Удаляем папку archive из исходной папки
+rm -rf archive
